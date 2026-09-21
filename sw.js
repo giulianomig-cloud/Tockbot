71e6840339ae61507ed10ce871f0c51f074fc0a8
giulianomig-cloud <giuliano.mig@gmail.com>
2026-09-21 22:54:21 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
 //
 // Cambia CACHE_VERSION ad ogni nuova build pubblicata (tienilo allineato ad
 // APP_VERSION in index.html) così le cache vecchie vengono scartate subito.
-const CACHE_VERSION = "v3-8";
+const CACHE_VERSION = "v3-9";
 const CACHE_NAME = "tock-cache-" + CACHE_VERSION;
 
 const APP_SHELL = ["./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
